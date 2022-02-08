9acd114d63b2dee27c77b0d6f7b61cadc3f56c00
Arian <arian_febrian_29rpl@student.smktelkom-mlg.sch.id>
2022-02-08 12:33:42 +0700

    First commit intent

---
+++ app/src/main/java/www/smktelkom/mlg/sch/id/latihanintent/MainActivity.java
@@ -0,0 +1,31 @@
+package www.smktelkom.mlg.sch.id.latihanintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_move_activity:
+                Intent moveIntent = new Intent(MainActivity.this,MoveActivity.class);
+                startActivity(moveIntent);
+                break;
+        }
+    }
+}
